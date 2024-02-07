29cd34e57f8f832326b2f0ea9b4ff6477aab93f6
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-07 20:33:27 +0530

    alternative way of creating reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,4 +1,9 @@
 import { createReducer } from "@ngrx/store";
 
 const initialState = 0
-export const counterReducer = createReducer(initialState)
+//export const counterReducer = createReducer(initialState)
+
+//Alternative way of creating reducer
+export function counterReducer(state = initialState){
+    return state;
+}
