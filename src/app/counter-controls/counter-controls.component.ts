b7fd39e42766beb735a4c2cb385c0ecd4a9eb4cd
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-08 21:36:54 +0530

    creating the decrement actions

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
+import { decrement, increment } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -17,6 +17,6 @@ export class CounterControlsComponent {
   }
 
   decrement() {
-
+    this.store.dispatch(decrement({value: 5}))
   }
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,17 +1,18 @@
 import { createReducer, on } from '@ngrx/store';
-import { increment } from './counter.actions';
+import { decrement, increment } from './counter.actions';
 
 
 const initialState = 0;
-// export const counterReducer = createReducer(
-//   initialState,
-//   on(increment, (state, action) => state + action.value)
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state, action) => state + action.value),
+  on(decrement, (state, action) => state - action.value)
+);
 
 //Alternative way of creating reducer
-export function counterReducer(state = initialState, action: any){
-  if(action.type === '[Counter] Increment'){
-    return state + action.value
-  }
-    return state;
-}
+// export function counterReducer(state = initialState, action: any){
+//   if(action.type === '[Counter] Increment'){
+//     return state + action.value
+//   }
+//     return state;
+// }
